a70ed06dcc0af370132f915c862ac83704727acc
tst <forevertangshutong@gmail.com>
2025-10-21 13:41:15 +0800

    使用vue-router

--- src/components/Music.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-<template>
-  <div class="not-found">
-    <h1>Music</h1>
-    <p>The page you are looking for does not exist.</p>
-  </div>
-</template>
-
-<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router/index.js'
+import AppLink from '@/components/AppLink.vue'
 
-createApp(App).mount('#app')
+createApp(App).component('AppLink', AppLink).use(router).mount('#app')

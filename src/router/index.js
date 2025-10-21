d1ef1b57cfc1ba873a54950a4d28c8e7111d59e6
tst <forevertangshutong@gmail.com>
2025-10-21 14:57:21 +0800

    none

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: import('@/views/Home.vue') },

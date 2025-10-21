c4882e68a0e4e546b6e92813f13a0cd077e06a58
tst <forevertangshutong@gmail.com>
2025-10-21 14:55:38 +0800

    优化一下视觉下效果

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,43 @@
 <script setup></script>
 <template>
-  <h1>HomePage</h1>
+  <div class="centered-rotated-text">TST</div>
+  <!--  <img class="fixed-sculpture" src="/headSculpture1.png" alt="sculpture" />-->
+  <img class="fixed-bluegiant" src="/BlueGiant-head-sculpture.png" alt="sculpture" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.centered-rotated-text {
+  opacity: 0.1;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  font-size: 70vw;
+  text-align: center;
+  font-weight: bold;
+  transform: translate(-50%, -50%) rotate(-15deg);
+  /*点击可以穿透*/
+  pointer-events: none;
+}
+.fixed-sculpture {
+  opacity: 0.1;
+  position: fixed;
+  left: 0; /* 贴左侧 */
+  bottom: 0; /* 贴底部 */
+  z-index: 999; /* 保证显示在最上层，不被其他元素覆盖 */
+  width: 50vw; /* 占据视口宽度的50% */
+  height: 100vh; /* 占据整个视口高度 */
+  object-fit: cover; /* 保持图片比例并覆盖整个区域 */
+}
+
+.fixed-bluegiant {
+  opacity: 0.2;
+  position: fixed;
+  right: 0; /* 贴左侧 */
+  top: 0; /* 贴底部 */
+  z-index: 999; /* 保证显示在最上层，不被其他元素覆盖 */
+  width: 60vw; /* 占据视口宽度的50% */
+  height: 100vh; /* 占据整个视口高度 */
+  object-fit: cover; /* 保持图片比例并覆盖整个区域 */
+  pointer-events: none;
+}
+</style>
--- src/views/Performance.vue
+++ src/views/Performance.vue
@@ -1,22 +1,24 @@
 <script setup></script>
 <template>
-  <h1>My favourtite things</h1>
-  <h2>Jonathan Kreisberg</h2>
-  <h3>Covered by tst</h3>
-  <video
-    controls
-    id="myVideo"
-    style="
-      width: 854px;
-      height: 480px;
-      object-fit: cover;
-      /* position: fixed;
+  <div style="display: flex; flex-direction: column; align-items: center;">
+    <h1>My favourtite things</h1>
+    <h2>Jonathan Kreisberg</h2>
+    <h3>Covered by tst</h3>
+    <video
+      controls
+      id="myVideo"
+      style="
+        width: 854px;
+        height: 480px;
+        object-fit: cover;
+        /* position: fixed;
       top: 0;
       left: 0; */
-    "
-  >
-    <source src="/rotate-My%20favourite%20things.mp4" type="video/mp4" />
-  </video>
+      "
+    >
+      <source src="/rotate-My%20favourite%20things.mp4" type="video/mp4" />
+    </video>
+  </div>
 </template>
 
 <style scoped></style>
